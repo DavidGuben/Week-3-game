cae28b7d080c5e45c9fa6ed7bce66b595812f53d
DavidGuben <davidjg6@gmail.com>
2016-05-22 00:29:55 -0400

    localizing bootstrap and fixing javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,107 +1,195 @@
-(function globalFunction () {
-    //FIRST IM DEFINING IMPORTANT VARIABLES GLOBALLY
-       var alphabet = "abcdefghijklmnopqrstuvwxyz";
-       var words = ["unicorn", "phoenix", "centaur", "mermaid","dragon","hydra","siren"];
-       var guessesLeft = 10;
-       var guessOutput = document.getElementById("guessOutput");
-       var hangedMan = document.getElementById("hangedMan");
-       var guessInput = document.getElementById("letter");
-       var lettersGuessed = lettersMatched = '';
-       var guessButton = document.getElementById("guess");
-       var numLettersMatched = 0;
-    //  NEXT I CREATE AN OBJECT WITH THE PROMPTS THAT WILL POP UP
-    function prompts() {
-        promptMessages = {
-            win: "<h3>You won</h3>",
-            lose: "<h3>Game over!</h3>",
-            guessedAlready: " <h3>You guessed that already</h3>",
-            validLetter: "<h3>Enter a valid letter</h3>"
-        };
-        //THEN I DISPLAY A WORD AT RANDOM AND LIMIT TO THE LENGTH OF THE RANDOM WORD
-        currentWord = words[Math.floor(Math.random() * words.length)];
-        //CREATING A VARIABLE THAT SUBTRACTS GUESSES FROM HANGEDMAN
-        hangedMan.innerHTML = 'You have ' + guessesLeft + ' guesses remaining';
-        guessOutput.innerHTML = '';
-        document.getElementById("letter").value = '';
-        var letters = document.getElementById("letters");
-        letters.innerHTML = '<li class="currentWord">Guess A Letter: </li>';
-        var letter, i;
-        for (i = 0; i < currentWord.length; i++) {
-            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
-            letters.insertAdjacentHTML('beforeend', letter);
+// global vars
+var prevWord = "";
+var currWord = "";
+var currGuesses = 0;
+var maxGuesses = 10;
+
+// elements
+var btnReset = document.getElementById("restart");
+var btnGuess = document.getElementById("guess");
+var output = document.getElementById("output");
+var man = document.getElementById("man");
+var letters = document.getElementById("letters");
+var guessInput = document.getElementById("letter");
+
+// event listeners
+btnReset.onclick = actionReset;
+btnGuess.onclick = actionGuess;
+
+// word associations
+var wordTemplates;
+var attempts = new Array();
+var numSolved = 0;
+var words = ["unicorn", "phoenix", "centaur", "mermaid", "dragon", "hydra", "siren"];
+
+// display messages
+var display = {
+    win: {
+        message: 'You win!',
+        className: "win"
+    },
+    lose: {
+        message: 'Game over!',
+        className: 'error'
+    },
+    guessed: {
+        message: 'You guessed that already',
+        className: 'error'
+    },
+    invalidLetter: {
+        message: 'Please enter a valid letter',
+        className: 'error'
+    },
+    clear: {
+        message: "",
+        className: ""
+    }
+};
+
+// start game on window init
+window.onload = actionReset;
+
+// resets the game
+function actionReset() {
+    resetWord();
+    resetView();
+    updateGuesses();
+}
+
+// event for each guess
+function actionGuess() {
+    // clearing any displays
+    setOutput(display.clear);
+
+    // collecting value
+    guess = guessInput.value.toLowerCase();
+
+    // making sure the char is between a & z
+    if (guess.length != 1 || guess.charCodeAt(0) < 97 ||  guess.charCodeAt(0) > 122) {
+        setOutput(display.invalidLetter);
+        return;
+    }
+
+    // checking if user already guessed this word
+    if (attempts.indexOf(guess) >= 0) {
+        setOutput(display.guessed);
+        return;
+    }
+
+    // incrementing num of guesses
+    attempts.push(guess);
+
+    // looking for matching letter in internal array
+    var isResolved = false;
+    for (var temp of wordTemplates) {
+        if (temp.letter == guess) {
+            temp.resolved = true;
+            temp.element.innerHTML = temp.letter.toUpperCase();
+            numSolved++;
+            isResolved = true;
         }
     }
-    //CREATING A IF/ELSE STATEMENT THAT DEFINES WIN OR LOSE
-    function gameOver(win) {
-        if (win) {
-            guessOutput.innerHTML = promptMessages.win;
-        } else {
-            guessOutput.innerHTML = promptMessages.lose;
-        }
-        //RESETS GUESS INPUT VALUE TO NOTHING IF WIN OR LOSE
-        guessInput.value = '';
+
+    // incrementing guesses if user didn't resolve a letter this round
+    if (!isResolved)
+        currGuesses++;
+
+    // updating the guesses display
+    updateGuesses();
+
+    // resetting element views
+    resetView();
+
+    // perform outcome check if max guesses or num solved is reached
+    if (maxGuesses - currGuesses <= 0 || currWord.length == numSolved) {
+        checkOutcome();
+        return;
     }
-    //STARTS THE GAME ON LOAD OF DOCUMENT
-    document.onload = prompts();
-    //CALLS THE GLOBAL FUNCTION TO RESET THE GAME
-    document.getElementById("restart").onclick = globalFunction;
-    //EVERYTIME YOU CLICK IN THE INPUT THIS WILL REPLACE THE VALUE OF THE INPUT TO NOTHING
-    //SO I DONT HAVE TO KEEP ERASING THE LETTER
-    guessInput.onclick = function () {
-        this.value = '';
-    };
-    //GUESS FUNCTION
-    document.getElementById('hangman').onsubmit = function (i) {
-        if (i.preventDefault) i.preventDefault();
-        guessOutput.innerHTML = '';
-        guess = guessInput.value.toLowerCase();
-        //IF STATEMENT THAT CHECKS THE GUESS
-        if (guess) {
-            //CHECK IF LETTER IS VALID
-            if (alphabet.indexOf(guess) > -1) {
-                //CHECK IF LETTER HAS BEEN GUESSED ALREADY
-                if ((lettersMatched && lettersMatched.indexOf(guess) > -1) || (lettersGuessed && lettersGuessed.indexOf(guess) > -1)) {
-                    guessOutput.innerHTML = '"' + guess.toUpperCase() + '"' + promptMessages.guessedAlready;
-                    //guessOutput.classList.add("warning");
-                }
-                //ADDS LETTERS TO OUTPUT IF GUESSED CORRECTLY
-                else if (currentWord.indexOf(guess) > -1) {
-                    var lettersToShow;
-                    lettersToShow = document.querySelectorAll(".letter" + guess.toUpperCase());
-                    for (var i = 0; i < lettersToShow.length; i++) {
-                        lettersToShow[i].classList.add("correct");
-                    }
-                    //CHECKS IF LETTER APPEARS MULTIPLE TIMES IN THE WORD
-                    for (var l = 0; l < currentWord.length; l++) {
-                        if (currentWord.charAt(l) === guess) {
-                            numLettersMatched += 1;
-                        }
-                    }
-                    //CHECKS IF NUMBERS OF LETTERS MATCHED EQUALS THE EXACT CURRENTWORD LENGTH SO IT
-                    //DISPLAYS A WINNING MESSAGE
-                    lettersMatched += guess;
-                    if (numLettersMatched === currentWord.length) {
-                        gameOver(true);
-                    }
-                }
-                //CHECKS GUESSES LEFT AND IF ITS 0 DISPLAYS A GAME OVER MESSAGE
-                else {
-                    lettersGuessed += guess;
-                    guessesLeft--;
-                    hangedMan.innerHTML = 'You have ' + guessesLeft + ' guesses remaining';
-                    if (guessesLeft === 0) gameOver();
-                }
-            }
-            //CHECKS IF LETTER IS VALID
-            else {
-                guessOutput.classList.add('error');
-                guessOutput.innerHTML = promptMessages.validLetter;
-            }
+
+    return false;
+}
+
+// checks if user won or lose
+function checkOutcome() {
+    if (currWord.length == numSolved)
+        setOutput(display.win);
+    else
+        setOutput(display.lose);
+
+    // hiding guess button
+    btnGuess.style.display = 'none';
+}
+
+// Function resets the current word and ensures it's not the same as the previous word
+function resetWord() {
+    // reset attemps
+    attempts = new Array();
+
+    // resetting current guesses
+    currGuesses = 0;
+
+    // resetting current num of solved
+    numSolved = 0;
+
+    // updating previous word
+    prevWord = currWord;
+
+    // choosing a new word that is not the same as the old word
+    while (currWord == prevWord)
+        currWord = words[Math.floor(Math.random() * words.length)]
+
+    // resetting the word templates array
+    wordTemplates = new Array();
+
+    // clearing list elements (the faster way)
+    while (letters.firstChild)
+        letters.removeChild(letters.firstChild);
+
+    for (var i=0; i<currWord.length; i++) {
+        // creating new list item and adding to DOM
+        ele = document.createElement("li");
+        ele.className = " letter ";
+        ele.appendChild(document.createTextNode(" "));
+        letters.appendChild(ele);
+
+        // creating internal letter association object
+        var template = {
+            "letter": currWord[i],  // curr letter
+            "element": ele,         // the element inside DOM
+            "resolved": false       // whether the letter has been chosen
         }
-        //CHECKS IF ANY LETTERS HAVE BEEN ENTERED
-        else {
-            guessOutput.classList.add('error');
-            guessOutput.innerHTML = promptMessages.validLetter;
-        }
-    };
-}());
+
+        // adding template for current letter to word templates array
+        wordTemplates.push(template);
+    }
+}
+
+// resets the elements' display
+function resetView() {
+    btnGuess.style.display = 'inline-block';
+    guessInput.value = "";
+    setOutput(display.clear);
+    guessInput.focus();
+}
+
+// sets the output display message
+function setOutput(status) {
+    output.innerHTML = '<h3>' + status.message + '<h3>';
+    output.className = "output " + status.className;
+    guessInput.focus();
+}
+
+// resetting the number of guesses
+function updateGuesses() {
+    var remaining = maxGuesses - currGuesses;
+    var classNames = ["text-danger", "text-warning", "text-info"];
+
+    if (remaining <= (maxGuesses/3))
+        className = classNames[0];
+    else if (remaining <= (maxGuesses/3*2))
+        className = classNames[1];
+    else
+        className = classNames[2];
+
+    man.innerHTML = 'You have <span class="'+className+'">'+remaining+'</span> guesses remaining';
+}
